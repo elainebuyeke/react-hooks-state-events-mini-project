72d6623370adac1651fb795e8e357d3b6f1a3535
Elaine <omwengaelaine@gmail.com>
2025-04-18 00:56:23 +0300

    complete

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,16 @@
-import React from "react";
 
-function TaskList() {
+import React from "react";
+import Task from "./Task";
+
+function TaskList({tasks, onDelete}) {
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task, index) => {
+        const {category, text} = task;
+        return (
+        <Task key={index} category={category} text={text} onDelete={onDelete} />
+        );
+      })}
     </div>
   );
 }
